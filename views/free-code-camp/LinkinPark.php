c9f55b82efe9be4a50f76cecb3a4e58651ec01fd
XemoTangch <jmphper@163.com>
2017-11-01 19:50:13 +0800

    add free code camp controller

---
+++ views/free-code-camp/LinkinPark.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * User:  jiangm
+ * Email: jmphper@foxmail.com
+ * Date:  2017/10/30
+ * Time:  22:49
+ * Desc:
+ */
+
+?>
+
+<link href="https://fonts.gdgdocs.org/css?family=Lobster" rel="stylesheet" type="text/css">
+<div class="container" style="margin: 60px auto;">
+    <div class="jumbotron">
+
+        <div class="row">
+            <div class="col-sx-12">
+                <div class="text-center"><h1 style="font-family: Lobster;">Linkin Park</h1></div>
+                <div class="text-center"><h3 style="font-family: Lobster;">Sometimes goodbye is the only way</h3><a href="http://music.163.com/#/song?id=16686390"><em>Shadow Of The Day</em></a></div>
+            </div>
+            <div class="col-xs-12">
+                <img class="img-thumbnail img-responsive center-block" src="https://gss3.bdstatic.com/-Po3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike150%2C5%2C5%2C150%2C50/sign=78fca10cc0ea15ce55e3e85bd7695196/00e93901213fb80e002bae5e3fd12f2eb8389445.jpg" alt="linkin park img" />
+            </div>
+        </div>
+
+        <div class="myRow row">
+            <div class="col-xs-2"><img class="img-thumbnail img-responsive" src="https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=d54f2f710ef3d7ca18fb37249376d56c/cdbf6c81800a19d8f153ec4a31fa828ba61e4628.jpg" alt="linkin park logo1"></div>
+            <div class="col-xs-10">
+                <p>林肯公园（Linkin Park）是一组来自美国加利福尼亚州的摇滚乐队，由乐队主唱查斯特·贝宁顿，麦克·信田、贝斯手菲尼克斯·法雷尔、吉他手布莱德·德尔森、鼓手罗伯·巴登和DJ采样手约瑟夫·韩组成。</p>
+            </div>
+        </div>
+
+        <div class="row myRow">
+            <div class="col-xs-10"><p>
+                    林肯公园在2000年以首张专辑《混合理论》（Hybrid Theory）在主流音乐市场上获得成功，专辑销售量超过2400万张。2002年11月发行第二张专辑《流星圣殿》（Meteora）。2002、2006年获得格莱美奖。2007年5月15日发行第三张专辑《末日警钟：毁灭·新生》（Minutes to Midnight）。2010年，林肯公园发行第四张专辑《烈日千阳》（A Thousand Suns）。2012年，林肯公园推出第五张专辑《生命·进化·原点》（Living Things），于6月26日全球发行。2014年6月13日，乐队发行第六张专辑《狩猎聚会》（The Hunting Party）。2017年5月19日，林肯公园发行第七张专辑《光芒再现》（One More Light）。同年7月20日，乐队主唱查斯特·贝宁顿在住宅内上吊自杀。
+                </p></div>
+            <div class="col-xs-2"><img class="img-thumbnail img-responsive" src="https://gss3.bdstatic.com/-Po3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=ba4649d4db1373f0e13267cdc566209e/5ab5c9ea15ce36d3be3c529f30f33a87e950b161.jpg" alt="linkin park logo2"></div>
+        </div>
+
+        <div class="row myRow">
+            <div class="col-xs-2"><img class="img-thumbnail img-responsive"src="https://gss0.bdstatic.com/-4o3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike180%2C5%2C5%2C180%2C60/sign=ad56ab0b237f9e2f6438155a7e598241/4e4a20a4462309f79c8fb4ff760e0cf3d6cad67e.jpg" alt="linkin park logo3"></div>
+            <div class="col-xs-10"><p>
+                    演艺活动外，林肯公园还助力于慈善事业。2005年创立公益组织“Music For Relief”（MFR）。2008年为四川地震灾区提供援助。2011年获得联合国基金会颁发的“全球领袖”奖。
+                </p></div>
+        </div>
+
+        <div class="row myRow">
+            <div class="col-xs-10"><p>
+                    林肯公园是我最爱的美国乐队，没有之一。没人知道查斯特到底为什么离开。 我们以为告别的只是查斯特-贝宁顿，其实还送走了林肯公园所创造的一个温柔摇滚的时代。 摇滚不只是愤怒，不只反抗，不只是如同飞鸟一般永不回头，而是呈现对这个复杂世界最丰富的理解。 这个男人忽然从尘世起身离开，世间再无林肯公园。
+                </p></div>
+            <div class="col-xs-2"><img class="img-thumbnail img-responsive" src="https://gss3.bdstatic.com/7Po3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike272%2C5%2C5%2C272%2C90/sign=4ab75aa8932397ddc274905638ebd9d2/9a504fc2d5628535ca52b16e92ef76c6a6ef63ca.jpg"></div>
+        </div>
+        <div class="row myRow">
+            <div class="col-xs-12"><p style="font-family: Lobster;">Good Goodbye Chester Bennington</p></div>
+            <div class="col-xs-12"><a href="https://linkinpark.com/home">Linkin Park</a></div>
+        </div>
+
+    </div>
+</div>
+
---
+++ views/layouts/normal.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * User:  jiangm
+ * Email: jmphper@foxmail.com
+ * Date:  2017/11/1
+ * Time:  19:35
+ * Desc:
+ */
+
+use app\widgets\Alert;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\CommonAsset;
+
+CommonAsset::register($this);
+?>
+
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<style type="text/css">
+    .footer {
+        height: 40px;
+        background-color: #f5f5f5;
+        border-top: 1px solid #ddd;
+        padding-top: 9px;
+    }
+</style>
+<body>
+<?php $this->beginBody() ?>
+
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+
+<?= $content ?>
+
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; Yii2 Study <?= date('Y') ?></p>
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
+
+
