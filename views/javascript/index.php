316829629cc17d1a9c6ebd94aa74a89d01b9e9b6
Xemo Tangch <jmphper@163.com>
2017-11-03 18:08:13 +0800

    add JavascriptController

---
+++ views/javascript/index.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Author: jiangm
+ * Email: jmphper@foxmail.com
+ * Date: 2017/11/3
+ * Time: 17:27
+ * Desc:
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Javascript Index</title>
+</head>
+<style type="text/css">
+    .table tbody tr td,.table tbody tr th{
+        vertical-align: middle;
+    }
+    .table tbody tr {
+        background-color: #f9f9f9;
+    }
+</style>
+<body>
+    <div class="container" style="min-height: 100%;">
+        <div class="row">
+            <div class="col-xs-12 text-center"><h2>Javascript List</h2></div>
+        </div>
+        <dvi class="row">
+            <div class="col-xs-12">
+
+                <table class="table table-bordered">
+                    <colgroup>
+                        <col class="col-xs-1">
+                        <col class="col-xs-7">
+                    </colgroup>
+                    <thead>
+                    <tr>
+                        <th>对象</th>
+                        <th>方法</th>
+                        <th>描述</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row" rowspan="2" class="text">
+                            <code>window</code>
+                        </th>
+                        <td><code>window.onbeforeunload</code></td>
+                        <td><a href="/javascript/index?type=onbeforeunload">离开页面时提示</a></td>
+                    </tr>
+                    <tr>
+                        <td><code>window.on</code></td>
+                        <td>鼠标悬停在行或单元格上时所设置的颜色</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </div>
+        </dvi>
+    </div>
+</body>
+</html>
