9d059ee008679809ea8e92eec9a21344592c91ae
XemoTangch <jmphper@163.com>
2017-11-28 23:20:40 +0800

    imhg data

---
+++ views/imhg-data/index.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * User:  jiangm
+ * Email: jmphper@foxmail.com
+ * Date:  2017/11/28
+ * Time:  22:55
+ * Desc:
+ */
+
+$this->title = '运营数据统计';
+?>
+
+<div class="wrap">
+    <nav class="navbar-inverse navbar-fixed-top navbar" id="navbar-homepage">
+        <div class="container-fluid">
+
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">移动端导航显示</span>
+                    <!-- 按钮样式 -->
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">APP运营数据</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li role="presentation" class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true" href="#">活跃用户漏斗图 <span class="caret"></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="">活跃用户漏斗图</a></li>
+                            <li><a href="">活跃用户漏斗图</a></li>
+                            <li><a href="">活跃用户漏斗图</a></li>
+                            <li><a href="">活跃用户漏斗图</a></li>
+                            <li><a href="">活跃用户漏斗图</a></li>
+                            <li><a href="">活跃用户漏斗图</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#contact">联系方式</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
+    
+
+</div>
