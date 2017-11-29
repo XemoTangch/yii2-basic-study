6cad8ddb83e68f8770204695989ee075fa85fbac
XemoTangch <jmphper@163.com>
2017-11-29 23:54:02 +0800

    imhg data edit

--- views/imhg-data/index.php
+++ views/imhg-data/index.php
@@ -28,18 +28,41 @@ $this->title = '运营数据统计';
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#contact">总数统计图</a></li>
                     <li role="presentation" class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true" href="#">活跃用户漏斗图 <span class="caret"></a>
+                        <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true" href="#">用户数据统计图<span class="caret"></a>
                         <ul class="dropdown-menu">
+                            <li><a href="">用户漏斗图</a></li>
                             <li><a href="">活跃用户漏斗图</a></li>
-                            <li><a href="">活跃用户漏斗图</a></li>
-                            <li><a href="">活跃用户漏斗图</a></li>
-                            <li><a href="">活跃用户漏斗图</a></li>
-                            <li><a href="">活跃用户漏斗图</a></li>
-                            <li><a href="">活跃用户漏斗图</a></li>
+                            <li><a href="">男女分布</a></li>
+                            <li><a href="">用户年龄分布</a></li>
+                            <li><a href="">用户留学国家</a></li>
+                            <li><a href="">用户兴趣爱好</a></li>
+                            <li><a href="">用户小喇叭</a></li>
                         </ul>
                     </li>
-                    <li><a href="#contact">联系方式</a></li>
+                    <li role="presentation" class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true" href="#">城市和定位统计<span class="caret"></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="">用户所在城市</a></li>
+                            <li><a href="">定位热点图</a></li>
+                            <li><a href="">定位热力图</a></li>
+                        </ul>
+                    </li>
+                    <li role="presentation" class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true" href="#">活动数据统计<span class="caret"></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="">活动类型分布</a></li>
+                            <li><a href="">活动排行</a></li>
+                        </ul>
+                    </li>
+                    <li role="presentation" class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true" href="#">其他统计<span class="caret"></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="">板块热度</a></li>
+                            <li><a href="">聊天数统计</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
