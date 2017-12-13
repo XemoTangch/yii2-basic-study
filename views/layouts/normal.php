c5b898c72c50899920d933a6213d44b02cfbf1e0
XemoTangch <jmphper@163.com>
2017-12-13 23:24:07 +0800

    js

--- views/layouts/normal.php
+++ views/layouts/normal.php
@@ -7,11 +7,7 @@
  * Desc:
  */
 
-use app\widgets\Alert;
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
 use app\assets\CommonAsset;
 
 CommonAsset::register($this);
