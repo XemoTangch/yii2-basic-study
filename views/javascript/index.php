274c1584298107f17eda7f935bd9954557c19d18
XemoTangch <jmphper@163.com>
2017-12-21 23:21:13 +0800

    no 3

--- views/javascript/index.php
+++ views/javascript/index.php
@@ -154,7 +154,7 @@
 </body>
 <script type="application/javascript">
     $(function(){
-        // 页面数据  no2
+        // 页面数据  no3
         var data = {
             knowledge_point: {
                 content:[
