11cb6c76b20a56ddc745edf5559f750a883a9e49
XemoTangch <jmphper@163.com>
2018-06-06 20:37:44 +0800

    add vue basic study

--- config/as_access.php
+++ config/as_access.php
@@ -14,5 +14,6 @@ return [
         'front/*',
         'demo/*',
         'authentication/*',
+        'admin/*',
     ],
 ];
---
+++ views/vue/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * User:  jiangm
+ * Email: jmphper@foxmail.com
+ * Date:  2018/06/06
+ * Time:  20:32
+ * Desc:
+ */
+
+/* @var $this yii\web\View */
+$this->title = 'vue 学习';
+?>
